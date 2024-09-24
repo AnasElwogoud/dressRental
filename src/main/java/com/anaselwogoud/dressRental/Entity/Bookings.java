1b691c361b4c0c791ad7a858d9ed580e80fe9f42
AnasElwogoud <anaselwogoud74@gmail.com>
2024-09-25 02:57:01 +0300

    first commit

---
+++ src/main/java/com/anaselwogoud/dressRental/Entity/Bookings.java
@@ -0,0 +1,29 @@
+package com.anaselwogoud.dressRental.Entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@ToString
+public class Bookings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull(message = "Date from is required")
+    private LocalDate dateFrom;
+    @Future(message = "Date To must be in Future")
+    private LocalDate dateTo;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private Users user;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "dress_id")
+    private Dress dress;
+}
