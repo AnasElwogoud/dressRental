5e3802006847920785c0543adea5ff47f32356d9
aelwogoud <aelwogoud@ntgclarity.com>
2024-10-08 15:16:52 +0300

    test

--- src/main/java/com/anaselwogoud/dressRental/Utils/Utils.java
+++ src/main/java/com/anaselwogoud/dressRental/Utils/Utils.java
@@ -16,15 +16,15 @@ public class Utils {
     private static final String ALPHANUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final SecureRandom secureRandom = new SecureRandom();
 
-//    public static String generateRandomConfirmationCode(int length) {
-//        StringBuilder stringBuilder = new StringBuilder();
-//        for (int i = 0; i < length; i++) {
-//            int randomIndex = secureRandom.nextInt(ALPHANUMERIC_STRING.length());
-//            char randomChar = ALPHANUMERIC_STRING.charAt(randomIndex);
-//            stringBuilder.append(randomChar);
-//        }
-//        return stringBuilder.toString();
-//    }
+    public static String generateRandomConfirmationCode(int length) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(ALPHANUMERIC_STRING.length());
+            char randomChar = ALPHANUMERIC_STRING.charAt(randomIndex);
+            stringBuilder.append(randomChar);
+        }
+        return stringBuilder.toString();
+    }
 
     public static UserDTO mapUserToUserDTO(Users user) {
         UserDTO userDTO = new UserDTO();
