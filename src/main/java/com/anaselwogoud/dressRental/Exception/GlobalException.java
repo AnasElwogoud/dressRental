5e2ff2a490aee7470415700062046d35a6b2dfe9
aelwogoud <aelwogoud@ntgclarity.com>
2024-10-09 18:45:36 +0300

    1st commit

--- src/main/java/com/anaselwogoud/dressRental/Exception/GlobalException.java
+++
@@ -1,8 +0,0 @@
-package com.anaselwogoud.dressRental.Exception;
-
-public class GlobalException extends RuntimeException {
-
-    public GlobalException(String message) {
-        super(message);
-    }
-}
